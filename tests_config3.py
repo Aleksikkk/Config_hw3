ffcfe5b376b3f631fbdea3801c1c506d5042451d
Алексей <alexlytkin2005@gmail.com>
2024-11-25 23:40:20 +0300

    Update tests_config3.py

--- tests_config3.py
+++ tests_config3.py
@@ -3,7 +3,7 @@ from config3 import *
 
 class TestConfigParser(unittest.TestCase):
     def test_parse_file(self):
-        data = parse_file('HW3/example_config.txt')
+        data = parse_file('HW3/example1_config.txt')
         self.assertEqual(data['port'], 8080)
         self.assertEqual(data['timeout'], 30)
         self.assertEqual(data['max_connections'], 100)
@@ -18,7 +18,24 @@ class TestConfigParser(unittest.TestCase):
         self.assertEqual(data['min_value'], 30)
         self.assertEqual(data['max_value'], 8080)
 
+    def test_monitoring_config(self):
+        data = parse_file('HW3/example2_config.txt')  
+
+        self.assertEqual(data['monitoring_service'], "HealthCheckService")
+        self.assertEqual(data['check_interval'], 5)
+
+        self.assertEqual(data['endpoints'], ["http://localhost:8080/health", "http://localhost:8080/status"])
+
+        self.assertEqual(data['notifications']['email'], "admin@example.com")
+        self.assertEqual(data['notifications']['sms'], "1234567890")
+        self.assertEqual(data['notifications']['alert_threshold'], 15)  
+
+        self.assertEqual(data['status']['last_check'], "2023-10-01T12:00:00Z")
+        self.assertEqual(data['status']['is_up'], True)
+        self.assertEqual(data['status']['response_time'], 150)  
+
 if __name__ == '__main__':
     unittest.main(exit=False)
-    print("Все тесты успешно пройдены!")
+
+
 
